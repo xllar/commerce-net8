4f59e52f18d260bf75e8c33ebb90e6f5903d2077
Xllar <ovicbananas@gmail.com>
2024-05-23 06:24:14 -0700

    End of commerce 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using Core.interfaces;
+using Core.Interfaces;
 using Infastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,9 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
@@ -26,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
